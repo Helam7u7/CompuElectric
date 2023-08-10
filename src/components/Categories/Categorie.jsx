78187320c5ab6dbadf265803c68762165472f8e6
Helam7u7 <helam053@gmail.com>
2023-08-10 16:54:13 -0500

    resolucion del fallo2

--- src/components/Categories/Categorie.jsx
+++ src/components/Categories/Categorie.jsx
@@ -11,7 +11,7 @@ const Categories = ({url}) => {
     </div>
     {/* <!-- cart categories --> */}
     <div className="categories__card">
-      <div className="card_img"><img src="/src/assets/img/laptop1.png" alt=""/></div>
+      <div className="card_img"><img src="/public/img/laptop1.png" alt=""/></div>
       <h3>LAPTOP MSI CYBORG 15 A12VF-043US</h3>
       <p>I7-12650H 8GB DDR5 512GB SSD...</p>
       <div className="info">
